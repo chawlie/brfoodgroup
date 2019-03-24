57a3983173e84a466148a0a97ce37e54b2cf8c1c
Charlie Chauvin <charliechauvin@gmail.com>
2019-03-23 22:12:50 -0500

    2nd commit adding all files

---
+++ parts/top-bar.php
@@ -0,0 +1,26 @@
+<div class="top-bar-container contain-to-grid show-for-medium-up">
+    <ul class="title-area no-bullet text-center">
+        <li class="logo">
+            <a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php bloginfo('template_url'); ?>/assets/img/brfoodgroup-logo.svg" alt="BR Food Group" title="BR Food Group"></a>
+        </li>
+    </ul>
+    <div class="row">
+        <div class="top-bar-bg small-12">
+            <nav class="top-bar center" data-topbar="">
+                <section class="top-bar-section">
+                    <?php foundationPress_top_bar_l(); ?>
+                    <?php foundationPress_top_bar_r(); ?>
+                </section>
+            </nav>
+        </div>
+    </div>
+    <div>
+                <h3 class="subnav-message" style="color: #333; /* background-color: rgba(4, 93, 113, 0.7); */
+    margin: 0 auto 15px;
+    font-size: 18px;
+    padding: 5px;
+    width: 75%;
+    font-weight:  700;
+">The BR Food Group is now producing clean dough kolaches!</h3>
+            </div>
+</div>
